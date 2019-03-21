9d7b1c0cb7d05a6ce277ea102eae8d01a9af325e
Yannick Lehmann <47973872+ylehmann@users.noreply.github.com>
2019-03-21 11:33:44 +0100

    Update autostart.sh

--- config/autostart.sh
+++ config/autostart.sh
@@ -1,4 +1,5 @@
-screen /opt/DataCollector/DataCollector -d
 docker-compose -f /opt/DataCollector/docker-compose.yml up -d
-sleep 20
+sleep 10
+screen /opt/DataCollector/DataCollector -d
+sleep 10
 chromium-browser http://localhost:3000 --start-fullscreen
